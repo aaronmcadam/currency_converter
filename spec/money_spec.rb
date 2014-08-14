cd9b1e1762d24b2d0f651ae6da48135e4ef79d4d
Aaron Mc Adam <aaron@aaronmcadam.com>
2014-08-14 23:16:30 +0100

    Implement addition for identical currencies (page 194, 195)

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,4 +1,6 @@
 class Money
+  attr_reader :amount, :currency
+
   def initialize(amount:, currency:)
     @amount = amount
     @currency = currency
@@ -8,19 +10,32 @@ class Money
     "#{amount} #{currency.upcase}"
   end
 
-  private
-
-  attr_reader :amount, :currency
+  def +(other_money)
+    self.class.new(amount: amount + other_money.amount, currency: currency)
+  end
 end
 
 describe Money do
   describe "#to_s" do
-    it "output the amount and the currency" do
+    it "outputs the amount and the currency" do
       money = Money.new(amount: 10, currency: :usd)
 
       result = money.to_s
 
       expect(result).to eq("10 USD")
+    end
+  end
+
+  describe "addition" do
+    context "when the currencies are the same" do
+      it "adds them together" do
+        ten_dollars = Money.new(amount: 10, currency: :usd)
+        twenty_dollars = Money.new(amount: 20, currency: :usd)
+
+        result = (ten_dollars + twenty_dollars).to_s
+
+        expect(result).to eq("30 USD")
+      end
     end
   end
 end
